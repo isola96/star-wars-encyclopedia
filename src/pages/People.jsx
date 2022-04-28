05f92389faaf0c1dba66c26e5390d61c458ea81e
Johanna Jönsson <johannajonsson96@hotmail.com>
2022-04-28 13:09:44 +0200

    updated People-page with correct imports

--- src/pages/People.jsx
+++ src/pages/People.jsx
@@ -1,24 +1,18 @@
-import axios from "axios"
+import Swapi from '../services/SWAPI'
 import { useState, useEffect } from "react"
-import Button from 'react-bootstrap/Button'
 import { Link } from 'react-router-dom'
+import { Button, Row, Col } from 'react-bootstrap'
 import { getIdFromUrl } from '../helpers/index'
 
 const People = () => {
-    const [people, setPeople] = useState([])
+    const [people, setPeople] = useState("")
     const [page, setPage] = useState(1)
 
     // Get people from API
     const getPeople = async () => {
-        try {
-            const res = await axios.get('https://swapi.dev/api/people')
-            const data = res.data.results
+            const data = await Swapi.getPeople()
             setPeople(data)
-            return data
-        } catch (err) {
-            return { message: err.message }
         }
-    }
 
     // Get people from API when component is first mounted
     useEffect(() => {
@@ -30,36 +24,36 @@ const People = () => {
     return (
         <>
             <h1>People</h1>
-            <div className='d-flex flex-wrap justify-content-center'>
-                {people.map(person => (
-                    <div key={person.name} className='card border-secondary m-3 col-3'>
-                        <div className='card-header'>
-                            <h2>{person.name}</h2>
+            <Row xs={1} md={2} lg={3}>
+            {people && people.results.map((person) => (
+                <Col>
+                    <div className="card"> 
+                        <div className="cardHeader">
+                            <h3>{person.name}</h3>
                         </div>
-                        <div className='card-body'>
-                            <p className='text-dark'>
-                                <span>Gender: </span>
-                                {person.gender}
-                            </p>
-                            <hr />
-                            <p className='text-dark'>
-                                <span>Born: </span>
-                                {person.birth_year}
-                            </p>
-                            <hr />
-                            <p className='text-dark'>
-                                <span>In: </span>
-                                {person.films.length}
-                            </p>
+                        <div className="cardBody">
+                            <Row>
+                                Gender: {person.gender}
+                            </Row>
+                            <Row>
+                                Born: {person.birth_year}
+                            </Row>
+                            <Row>
+                                In: {person.films.length} films
+                            </Row>
+                        </div>
+
+                        <div className='buttonWrapper'>
                             <Button 
                                 variant="primary" 
-                                as={Link}
+                                as={Link} 
                                 to={`/people/${getIdFromUrl(person.url)}`}
                             >Read more</Button>
                         </div>
                     </div>
-                ))}
-            </div>
+                </Col>
+            ))}
+            </Row>
             <div className="d-flex justify-content-between align-items-center mt-4">
 			<div className="prev">
 				<Button
