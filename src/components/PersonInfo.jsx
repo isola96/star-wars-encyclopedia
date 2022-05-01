4457ef04cc0a5ef4adbf3ee1ba006065ed2ad68c
Johanna Jönsson <johannajonsson96@hotmail.com>
2022-05-01 16:45:12 +0200

    moved people and person info to component

---
+++ src/components/PersonInfo.jsx
@@ -0,0 +1,154 @@
+import SwapiAPI from '../services/SWAPI'
+import { useState, useEffect } from "react"
+import { Row, Col, Button } from 'react-bootstrap'
+import { useParams } from 'react-router-dom'
+import { Link } from 'react-router-dom'
+import { getIdFromUrl } from '../helpers/index'
+
+const PersonInfo = () => {
+    const [person, setPerson] = useState("")
+    const { id } = useParams()
+    const [films, setFilms] = useState([])
+    const [error, setError] = useState("")
+    const [loading, setLoading] = useState(false)
+
+    // Get one person
+    const getPerson = async (id) => {
+        setLoading(true) 
+        try {
+            const data = await SwapiAPI.getOnePerson(id)
+            setPerson(data)
+            setFilms(data.films)
+            setLoading(false)
+        } catch (err) {
+            setError(err.message)
+        }
+    }
+
+    // Get person from API when component is first mounted
+    useEffect(() => {
+        getPerson(id)
+    }, [id])
+
+    return (
+        <>
+            {error && {error}}
+
+            {loading && !person && (
+                <h3>Loading...</h3>
+            )}
+
+            {person && (
+                <Row>
+                    <Col>
+                    <div className="card">
+                            <div className="cardHeader"><h2>{person.name}</h2></div>
+                            <div className="cardBody">
+                                <Row className="cardRow" md={1}>
+                                    <Col>
+                                        <h3>Attributes </h3>
+                                    </Col>
+                                </Row>
+
+                                <Row className="cardRow" md={3}>
+                                    <Col className="cardColLeft">
+                                        <p>Gender </p>
+                                    </Col>
+                                    <Col className="cardColRight">
+                                        <p>{person.gender}</p>
+                                    </Col>
+                                </Row>
+
+                                <Row className="cardRow" md={3}>
+                                    <Col className="cardColLeft">
+                                        <p>Birth year </p>
+                                    </Col>
+                                    <Col className="cardColRight">
+                                        <p>{person.birth_year}</p>
+                                    </Col>
+                                </Row>
+
+                                <Row className="cardRow" md={3}>
+                                    <Col className="cardColLeft">
+                                        <p>Height </p>
+                                    </Col>
+                                    <Col className="cardColRight">
+                                        <p>{person.height}</p>
+                                    </Col>
+                                </Row>
+
+                                <Row className="cardRow"  md={3}>
+                                    <Col className="cardColLeft">
+                                        <p>Mass </p>
+                                    </Col>
+                                    <Col className="cardColRight">
+                                        <p>{person.mass}</p>
+                                    </Col>
+                                </Row>
+
+                                <Row className="cardRow"  md={3}>
+                                    <Col className="cardColLeft">
+                                        <p>Hair color </p>
+                                    </Col>
+                                    <Col className="cardColRight">
+                                        <p>{person.hair_color}</p>
+                                    </Col>
+                                </Row>
+
+                                <Row className="cardRow"  md={3}>
+                                    <Col className="cardColLeft">
+                                        <p>Skin color </p>
+                                    </Col>
+                                    <Col className="cardColRight">
+                                        <p>{person.skin_color}</p>
+                                    </Col>
+                                </Row>
+
+                                <Row className="cardRow"  md={3}>
+                                    <Col className="cardColLeft">
+                                        <p>Eye color </p>
+                                    </Col>
+                                    <Col className="cardColRight">
+                                        <p>{person.eye_color}</p>
+                                    </Col>
+                                </Row>
+
+                                <Row className="cardRow" md={1}>
+                                    <Col>
+                                        <h3>Links </h3>
+                                    </Col>
+                                </Row>
+
+                                <Row className="cardRow"  md={3}>
+                                    <Col className="cardColLeft">
+                                        <p>Films </p>
+                                    </Col>
+
+                                    <Col className="cardColRight">
+                                        {films.map(films => (
+                                            <Link   
+                                                to={`/films/${getIdFromUrl(films)}`} 
+                                                key={getIdFromUrl(films)}>
+                                                <li>Film {getIdFromUrl(films)}</li>
+                                            </Link>
+                                        ))} 
+                                    </Col>
+                                </Row>
+                            </div>
+                            <div className='buttonWrapper'>
+                                <Button 
+                                    className="btn"
+                                    variant="outline-dark" 
+                                    as={Link} 
+                                    to={'/people'}
+                                >Back</Button>
+                            </div>
+                        </div>
+                    </Col>
+                </Row>
+            )}
+        </>
+    )
+}
+
+export default PersonInfo
--- src/pages/People.jsx
+++ src/pages/People.jsx
@@ -1,89 +1,9 @@
-import Swapi from '../services/SWAPI'
-import { useState, useEffect } from "react"
-import { Link } from 'react-router-dom'
-import { Button, Row, Col } from 'react-bootstrap'
-import { getIdFromUrl } from '../helpers/index'
+import PeopleInfo from "../components/PeopleInfo"
 
 const People = () => {
-    const [people, setPeople] = useState("")
-    const [page, setPage] = useState(1)
-    const [error, setError] = useState("")
-    const [loading, setLoading] = useState(false)
-
-    // Get people from API
-    const getPeople = async (page) => {
-            setLoading(true)
-            try {
-                const data = await Swapi.getPeople(page)
-                setPeople(data)
-                setLoading(false)
-            } catch (err) {
-                setError(err.message)
-            }
-        }
-
-    // Get people from API when component is first mounted
-    useEffect(() => {
-        getPeople(page)
-    }, [page])
-
     return (
         <>
-            <h1>People</h1>
-            <Row xs={1} md={2} lg={3}>
-            
-            {error && {error}}
-
-            {loading && !people && (
-                <h3>Loading...</h3>
-            )}
-
-            {people && people.results.map((person, index) => (
-                <Col key={index}>
-                    <div className="card"> 
-                        <div className="cardHeader">
-                            <h3>{person.name}</h3>
-                        </div>
-                        <div className="cardBody">
-                            <Row>
-                                Gender: {person.gender}
-                            </Row>
-                            <Row>
-                                Born: {person.birth_year}
-                            </Row>
-                            <Row>
-                                In: {person.films.length} films
-                            </Row>
-                        </div>
-
-                        <div className='buttonWrapper'>
-                            <Button 
-                                variant="outline-dark" 
-                                as={Link} 
-                                to={`/people/${getIdFromUrl(person.url)}`}
-                            >Read more</Button>
-                        </div>
-                    </div>
-                </Col>
-            ))}
-            </Row>
-            <div className="d-flex justify-content-between align-items-center mt-4">
-                <div className="prev">
-                    <Button
-                        disabled={!people.previous || page <= 1}
-                        onClick={() => setPage(prevValue => prevValue - 1)}                        
-                        variant="light"
-                    >Previous Page</Button>
-                </div>
-                <div className="page">{page} / 9 </div>
-                <div className="next">
-                    <Button
-                        disabled={!people.next || page >= 9}
-                        onClick={() => setPage(prevValue => prevValue + 1)}
-                        variant="light"
-                    >Next Page</Button>
-                </div>
-		    </div>
+            <PeopleInfo />
         </>
     )
 }
