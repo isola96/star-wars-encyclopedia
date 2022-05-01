08c3c8bc68c65ccbfa3d9d2cc14d4db975314688
Johanna Jönsson <johannajonsson96@hotmail.com>
2022-05-01 16:45:00 +0200

    moved film and films info to component

---
+++ src/components/FilmInfo.jsx
@@ -0,0 +1,120 @@
+import SwapiAPI from '../services/SWAPI'
+import { useState, useEffect } from "react"
+import { Row, Col, Button } from 'react-bootstrap'
+import { useParams } from 'react-router-dom'
+import { Link } from 'react-router-dom'
+import { getIdFromUrl } from '../helpers/index'
+
+const FilmInfo = () => {
+    const [film, setFilm] = useState([])
+    const { id } = useParams()
+    const [characters, setCharacters] = useState([])
+    const [error, setError] = useState("")
+    const [loading, setLoading] = useState(false)
+
+    // Get one film
+    const getFilm = async (id) => {
+        setLoading(true) 
+        try {
+            const data = await SwapiAPI.getOneFilm(id)
+            setFilm(data)
+            setCharacters(data.characters)
+            setLoading(false)
+        } catch (err) {
+            setError(err.message)
+        }
+	}
+
+    // Get film from API when component is first mounted
+	useEffect(() => {
+		getFilm(id)
+	}, [id])
+
+    return (
+        <>
+            {error && {error}}
+
+            {loading && !film && (
+                <h3>Loading...</h3>
+            )}
+
+            {film && (
+                <Row>
+                    <Col>
+                        <div className="card">
+                            <div className="cardHeader"><h2>{film.title}</h2></div>
+                            <div className="cardBody">
+                                <Row className="cardRow" md={3}>
+                                    <Col className="cardColLeft">
+                                        <p>Episode: </p>
+                                    </Col>
+                                    <Col className="cardColRight">
+                                        <p>{film.episode_id}</p>
+                                    </Col>
+                                </Row>
+
+                                <Row className="cardRow" md={3}>
+                                    <Col className="cardColLeft">
+                                        <p>Director: </p>
+                                    </Col>
+                                    <Col className="cardColRight">
+                                        <p>{film.director}</p>
+                                    </Col>
+                                </Row>
+
+                                <Row className="cardRow" md={3}>
+                                    <Col className="cardColLeft">
+                                        <p>Producer: </p>
+                                    </Col>
+                                    <Col className="cardColRight">
+                                        <p>{film.producer}</p>
+                                    </Col>
+                                </Row>
+
+                                <Row className="cardRow"  md={3}>
+                                    <Col className="cardColLeft">
+                                        <p>Release date: </p>
+                                    </Col>
+                                    <Col className="cardColRight">
+                                        <p>{film.release_date}</p>
+                                    </Col>
+                                </Row>
+
+                                <Row className="cardRow" md={1}>
+                                    <Col>
+                                        <h3>Characters in this film:</h3>
+                                    </Col>
+                                </Row>
+
+                                <Row className="cardRow" md={3}>
+                                    <Col className="cardColLeft">
+                                        <p>Characters</p>
+                                    </Col>
+
+                                    <Col className="cardColRight">
+                                        {characters.map(character => (
+                                            <Link to={`/people/${getIdFromUrl(character)}`} key={character}>
+                                                <li>Character {getIdFromUrl(character)}</li>
+                                            </Link>
+                                        ))} 
+                                    </Col>
+                                </Row>
+
+                            </div>
+                            <div className='buttonWrapper'>
+                                <Button 
+                                    className="btn" 
+                                    variant="outline-dark"
+                                    as={Link} 
+                                    to={'/films'}
+                                >Back</Button>
+                            </div>
+                        </div>
+                    </Col>
+                </Row>  
+            )}
+        </>
+    )
+}
+
+export default FilmInfo
--- src/pages/Films.jsx
+++ src/pages/Films.jsx
@@ -1,89 +1,9 @@
-import SwapiApi from "../services/SWAPI"
-import { getIdFromUrl } from '../helpers/index'
-import { useState, useEffect } from "react"
-import { Link } from 'react-router-dom'
-import { Button, Row, Col } from 'react-bootstrap'
+import FilmsInfo from "../components/FilmsInfo"
 
 const Films = () => {
-    const [films, setFilms] = useState("")
-    const [page, setPage] = useState(1)
-    const [error, setError] = useState("")
-    const [loading, setLoading] = useState(false)
-
-    // Get films from API
-	const getFilmsFromSwapi = async () => {
-        setLoading(true)
-        try {
-            const data = await SwapiApi.getFilms()
-		    setFilms(data)
-            setLoading(false)
-        } catch (err) {
-            setError(err.message)
-        }
-	}
-
-    // Get films from API when component is first mounted
-	useEffect(() => {
-		getFilmsFromSwapi()
-	}, [])  
-
     return (
-       <>
-        <h1>Films</h1>
-        <Row xs={1} md={2} lg={3}>
-        {error && {error}}
-
-        {loading && (
-            <h3>Loading...</h3>
-        )}
-
-        {films && films.results.map((film, index) => (
-            <Col key={index}>
-                <div className="card"> 
-                    <div className="cardHeader">
-                        <h3>{film.title}</h3>
-                    </div>
-
-                    <div className="cardBody">
-                        <Row>
-                            Episode {film.episode_id}
-                        </Row>
-                        <Row>
-                            Released {film.release_date}
-                        </Row>
-                        <Row>
-                            {film.characters.length} characters
-                        </Row>
-                    </div>
-
-                    <div className='buttonWrapper'>
-                        <Button 
-                            variant="outline-dark"
-                            as={Link} 
-                            to={`/films/${getIdFromUrl(film.url)}`}
-                        >Read more</Button>
-                    </div>
-                </div>
-            </Col>
-        ))}
-        </Row>
-        <div className="d-flex justify-content-between align-items-center mt-4">
-            <div className="prev">
-                <Button
-                    disabled={page === 1}
-                    onClick={() => setPage(prevValue => prevValue - 1)}
-                    variant="light"
-                >Previous Page</Button>
-            </div>
-            <div className="page">{page} / 1</div>
-            <div className="next">
-                <Button
-                    disabled={page + 1 >= page}
-                    onClick={() => setPage(prevValue => prevValue + 1)}
-                    variant="light"
-                >Next Page</Button>
-            </div>
-        </div>
+        <>
+            <FilmsInfo />
         </>
     )
 }
